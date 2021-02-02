3a5bec91a0f697e12f2d25a8b1d0827b9700ffe5
Luke Song <lsong@overlake.org>
2021-02-02 00:11:52 -0800

    More updates

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,8 +1,8 @@
-print("Escape codes: \\\\")
-print("Hello World")
-print('''Print
+print("Here is some text")
+print('''Here is more text
+except 
 on
 multiple
 lines''')
-print("The file is stored in C:\\new folder")
-print("More \nwords \non \nmultiple \nlines")
+print("Here is \neven \nmore \ntext \non \nmultiple \nliness")
+print("This is a backslash: \\")
