9df02e3f52633296b60867582df2fe4191899d0b
Luke Song <lsong@overlake.org>
2021-02-02 00:03:25 -0800

    New updates

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,8 @@
 print("Escape codes: \\\\")
 print("Hello World")
+print('''Print
+on
+multiple
+lines''')
+print("The file is stored in C:\\new folder")
+print("More \nwords \non \nmultiple \nlines")
