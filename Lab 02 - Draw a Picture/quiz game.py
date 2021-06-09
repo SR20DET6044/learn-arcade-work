32d2642bcea26a09e27e90124bf546553af419d6
Luke Song <lsongsr20@gmail.com>
2021-06-08 23:09:15 -0700

    quiz game 1

--- Lab 02 - Draw a Picture/quiz game.py
+++ Lab 02 - Draw a Picture/quiz game.py
@@ -1,6 +1,28 @@
 #welcome
 
-print("Hello! Welcome to my quiz!")
+
+def welcome1():
+    print("Hello! Welcome to my car quiz!")
+    print()
+
+def correct1():
+    print("Correct!")
+    print()
+
+def incorrect1():
+    print("Incorrect")
+    print()
+
+def completion1():
+    print("You have completed the quiz!")
+
+def score1():
+    print("You have answered",points,"questions correctly.")
+    print("Your score is", percent, "percent!")
+
+
+welcome1()
+
 
 #name input
 name = input("What is your name? ")
@@ -11,94 +33,74 @@ questions = 0
 
 questions += 1
 
-answer = input("What is 1+1? ")
+answer = input("What is the stock horsepower of the Toyota 2JZ-GTE? ")
 
-if answer == "2":
-    print("Correct!")
+if answer == "320":
+    correct1()
     points += 1
 
 else:
-    print("Incorrect")
+    incorrect1()
 
 #question 2
 
 questions += 1
 
-answer = input("What is 2+2? ")
+answer = input("What is the abbreviation for Honda's variable valve timing system? ")
 
-if answer == "4":
-    print("Correct!")
-    points += 1
+if answer.upper() == "VTEC":
+        correct1()
+        points += 1
 
 else:
-    print("Incorrect")
+    incorrect1()
 
 #question 3
 
 questions += 1
 
-answer = input("What is 3+3? ")
+answer = input("What is the cubic centimeter displacement of the RB20DET? ")
 
-if answer == "6":
-    print("Correct!")
+if answer == "1998":
+    correct1()
     points += 1
 
 else:
-    print("Incorrect")
+    incorrect1()
 
 #question 4
 
 questions += 1
 
-answer = input("What is 4+4?")
+answer = input("What is the name of the four wheel drive system found in many older Nissans? ")
 
-if answer == "8":
-    print("Correct!")
-    points += 1
+if answer.upper() == "ATTESA":
+        correct1()
+        points += 1
 
 else:
-    print("Incorrect")
+    incorrect1()
 
 #question 5
 
 questions += 1
 
-answer = input("What is 5+5?")
+answer = input("What is the name of the four wheel steering system found in many older Nissans? ")
 
-if answer == "10":
-        print("Correct!")
+if answer.lower() == "hicas":
+        correct1()
         points += 1
 
 else:
-        print("Incorrect")
-
-#question 6
-
-questions += 1
-
-answer = input("What is the capital of Japan? ")
-
-if answer.lower() == "tokyo":
-        print("Correct!")
-        points += 1
-
-else:
-        print("Incorrect")
-
-#question 6
-
-questions += 1
-
-answer = input("What is the capital of France? ")
-
-if answer.upper() == "PARIS":
-        print("Correct!")
-        points += 1
-
-else:
-        print("Incorrect")
+        incorrect1()
 
 
-print("You have completed the quiz!")
+completion1()
 
-print("Your score is", int(points/questions*100), "percent!")
+percent = int(points/questions*100)
+
+score1()
+
+if percent == 100:
+    for i in range(3):
+        print("Hooray!")
